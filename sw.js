ba48a41742d97a316747ad250e02b43480433585
CompuWiser <compuwiser@outlook.com>
2019-09-16 22:19:46 +0300

    Lesson 15: Nearly all assets cached

--- sw.js
+++ sw.js
@@ -1,6 +1,30 @@
+const staticCacheName = 'site-static';
+const assets = [
+  './',
+  './index.html',
+  './js/app.js',
+  './js/ui.js',
+  './css/styles.css',
+  './img/dish.png',
+  './img/icons/favicon.ico',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
 // install event
 self.addEventListener('install', (evt) => {
-  console.log('service worker installed');
+  //console.log('service worker installed');
+
+  evt.waitUntil(
+    caches
+      .open(staticCacheName)
+      .then((cache) => {
+        console.log('caching shell assets');
+        cache.addAll(assets);
+      })
+  );
 });
 
 // activate event
@@ -10,5 +34,5 @@ self.addEventListener('activate', (evt) => {
 
 // fetch event
 self.addEventListener('fetch', (evt) => {
-  console.log({url: evt.request.url, method: evt.request.method});
+  console.log({ url: evt.request.url, method: evt.request.method });
 });
